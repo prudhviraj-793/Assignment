bf032202a1a2e1ac26393d447e1d6adc7c32bf72
Prudhvi Raj <prudhvi@123.com>
2022-11-27 22:59:58 +0530

    spinWheel completed now

--- src/Components/SpinWheel.js
+++ src/Components/SpinWheel.js
@@ -1,15 +1,27 @@
 import React from "react";
 import spinWheel from "../Images/Group 1579.png";
 import "../CSS/SpinWheel.css";
+import { useState } from "react";
+import pointer from "../Images/Group 1577.png";
 
 function SpinWheel() {
+  const [state, setState] = useState(false);
+  function spinHandler(e) {
+    e.preventDefault();
+    setState(!state);
+  }
   return (
     <div className="spinWheel-container">
-      <div className="spinWheel-img">
-        <img src={spinWheel} alt="wheel" />
+      <div className="spinWheel-img ">
+        <img
+          className={state ? "spin-wheel" : ""}
+          src={spinWheel}
+          alt="wheel"
+        />
+        <img className="pointer" src={pointer} alt="pointer" />
       </div>
       <div className="spin-btn">
-        <button>SPIN</button>
+        <button onClick={spinHandler}>SPIN</button>
       </div>
     </div>
   );
